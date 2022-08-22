ce4b542974daac529655e9de9a3419656d7671aa
unknown <arfinchowdhuryarif7@gmail.com>
2022-08-22 19:00:51 +0600

    reducer implement running

--- src/components/redux/todo/reducer.js
+++ src/components/redux/todo/reducer.js
@@ -1,3 +1,15 @@
-export const reducer = (state) =>{
+import { ADDED } from "./actionTypes";
+import { initialValue } from "./initialState";
 
-}
+const todoReducer = (state = initialValue, action) => {
+    switch(action.type){
+        case ADDED:
+            return{
+
+            }
+
+            default: return state;
+    }
+};
+
+export default todoReducer;
