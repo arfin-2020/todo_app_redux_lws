00bcd6ce5abbf478fcbfffd1ba9fff810fc89cf6
unknown <arfinchowdhuryarif7@gmail.com>
2022-08-22 16:01:47 +0600

    html to react convert complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Header from "./components/Header";
+import Todo from "./components/Todo";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
+        <NavBar />
+        <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+          <Header />
+          <hr className="mt-4" />
+          <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
+            <Todo />
+          </div>
+          <Footer/>
+          <hr className="mt-4" />
+        </div>
+      </div>
     </div>
   );
 }
