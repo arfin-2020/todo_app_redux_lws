af4f054d9e7e92c21511e6a0799694d3da805292
unknown <arfinchowdhuryarif7@gmail.com>
2022-08-22 20:00:43 +0600

    reducer something done

--- src/components/redux/todo/reducer.js
+++ src/components/redux/todo/reducer.js
@@ -1,12 +1,35 @@
-import { ADDED } from "./actionTypes";
+import { ADDED, TOGGLED } from "./actionTypes";
 import { initialValue } from "./initialState";
 
+
+const nextTodoId = (todos) =>{
+    const maxId = todos.reducer((maxId, todo)=>Math.max(todo.id, maxId),-1);
+    console.log("maxID----------",maxId+1);
+    return maxId +1;
+}
 const todoReducer = (state = initialValue, action) => {
     switch(action.type){
         case ADDED:
-            return{
+            return[
+                ...state,
+                {
+                    id:nextTodoId(state),
+                    text: action.payload,
 
-            }
+                }
+            ]
+            case TOGGLED:
+                return state.map((todo)=>{
+                    if(todo.id !== action.payload){
+                        return todo;
+                    }else{
+                        return{
+                            ...todo,
+                            completed: !completed,
+                        }
+                    }
+                })
+            
 
             default: return state;
     }
